d4469787b18e6f617f92dc14e0f8f6978c5f04e4
Karna Sravan <ext-skarna@exo.inc>
2026-03-17 21:12:18 +0530

    initial commit

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now(),
+        'hostname': socket.gethostname()
+    }
+    )
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'up'
+    }
+    ),200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
